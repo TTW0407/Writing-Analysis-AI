b306961166ece3e872e4069b8ed6fdd7d82b551c
TTW0407 <ttwei0407@gmail.com>
2024-11-13 11:11:56 +0800

    Change Model

--- learning_ai.py
+++ learning_ai.py
@@ -23,7 +23,7 @@ genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 
 #Create Model
-model = genai.GenerativeModel("gemini-1.5-flash",
+model = genai.GenerativeModel("gemini-1.5-pro",
     system_instruction=f"""Please communicate with users in {language}.
 
     Task guidelines:
